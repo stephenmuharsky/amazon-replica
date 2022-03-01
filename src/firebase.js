d9fa8cd324f8c24922bc954413c0ac67fce03796
stephenmuharsky <50891974+stephenmuharsky@users.noreply.github.com>
2022-03-01 10:47:32 -0500

    Set Up

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAA7zfT5wT7YHuG9UPv-AgYcGzw1taChcw",
+  authDomain: "clone-57593.firebaseapp.com",
+  projectId: "clone-57593",
+  storageBucket: "clone-57593.appspot.com",
+  messagingSenderId: "881719108422",
+  appId: "1:881719108422:web:991fffe62957061584fb47",
+};
